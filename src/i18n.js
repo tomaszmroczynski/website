f09c8496df0885eb57729a2ca765bd8689be25b5
tomaszmroczynski@gmail.com <tomaszmroczynski@gmail.com>
2022-01-16 21:18:13 +0100

    auto detekcja jezyka

--- src/i18n.js
+++ src/i18n.js
@@ -2,16 +2,18 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import XHR from "i18next-http-backend"
 
-i18n
-  .use(Backend)
+
+i18n.use(XHR).use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "pl",
+    fallbackLng: "en",
+    supportedLngs: ['en', 'no', 'pl'],
     debug: true,
     detection: {
-      order: ["queryString", "cookie"],
+      order: ["queryString", "navigator", "cookie"],
       cache: ["cookie"],
     },
     interpolation: {
@@ -20,3 +22,29 @@ i18n
   });
 
 export default i18n;
+
+
+
+
+
+{/* nowe
+i18n
+  .use(XHR) // <---- add this
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    // lng: 'en' // <--- turn off for detection to work
+    detection: options,
+    resources,
+    ns: ['common'],
+    defaultNS: 'common',
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'no', 'pl'],
+    interpolation: {
+      escapeValue: false,
+    },
+    debug: false,
+  })
+
+export default i18n
+*/}
