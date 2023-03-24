92a4603c671c6bcf3f42e2ce0727afffc5a7af14
Tomasz Mroczynski <tomaszmroczynski@gmail.com>
2023-03-25 00:34:45 +0100

    gdf

--- src/pages/lazMoss.js
+++ src/pages/lazMoss.js
@@ -93,11 +93,11 @@ class Case extends Component {
                     hasBackground={this.state.hasBackground}
                   >
                     <BackArrow src="./img/back.png" alt="Back to Projects" />
-                    <span><Trans i18nKey={"domDrammen.2428"}></Trans></span>
+                    <span><Trans i18nKey={"lazMoss.2428"}></Trans></span>
                   </BackButton>
-                  <ClientTag><Trans i18nKey={"domDrammen.2504"}></Trans></ClientTag>
-                  <Title><Trans i18nKey={"domDrammen.2551"}></Trans></Title>
-                  <TechniqueTitle><Trans i18nKey={"domDrammen.2613"}></Trans></TechniqueTitle>
+                  <ClientTag><Trans i18nKey={"lazMoss.2504"}></Trans></ClientTag>
+                  <Title><Trans i18nKey={"lazMoss.2551"}></Trans></Title>
+                  <TechniqueTitle><Trans i18nKey={"lazMoss.2613"}></Trans></TechniqueTitle>
                 </div>
                 
               </Hero>
@@ -108,42 +108,42 @@ class Case extends Component {
               <div className="row m-2-hor">
                 <div className="col-md-8">
                   
-                  <p className="content"><span className="color"><Trans i18nKey={"domDrammen.3209"}></Trans> </span><Trans i18nKey={"domDrammen.3210"}></Trans></p>
-                  <p className="content"><span className="color"><Trans i18nKey={"domDrammen.3696"}></Trans> </span><Trans i18nKey={"domDrammen.3697"}></Trans></p>
-                  <p className="content"><span className="color"><Trans i18nKey={"domDrammen.3699"}></Trans> </span><Trans i18nKey={"domDrammen.3698"}></Trans></p>
+                  <p className="content"><span className="color"><Trans i18nKey={"lazMoss.3209"}></Trans> </span><Trans i18nKey={"lazMoss.3210"}></Trans></p>
+                  <p className="content"><span className="color"><Trans i18nKey={"lazMoss.3696"}></Trans> </span><Trans i18nKey={"lazMoss.3697"}></Trans></p>
+                  <p className="content"><span className="color"><Trans i18nKey={"lazMoss.3699"}></Trans> </span><Trans i18nKey={"lazMoss.3698"}></Trans></p>
                 </div>
                 <div className="col-md-4 sticky">
                   <ul className="detailproject">
                     <li>
-                      <span className="tile"><Trans i18nKey={"domDrammen.4204"}></Trans></span>
-                      <span><Trans i18nKey={"domDrammen.4252"}></Trans></span>
+                      <span className="tile"><Trans i18nKey={"lazMoss.4204"}></Trans></span>
+                      <span><Trans i18nKey={"lazMoss.4252"}></Trans></span>
                     </li>
                     <li>
-                      <span className="tile"><Trans i18nKey={"domDrammen.4368"}></Trans></span>
-                      <span><Trans i18nKey={"domDrammen.4412"}></Trans></span>
+                      <span className="tile"><Trans i18nKey={"lazMoss.4368"}></Trans></span>
+                      <span><Trans i18nKey={"lazMoss.4412"}></Trans></span>
                     </li>
                     <li>
-                      <span className="tile"><Trans i18nKey={"domDrammen.4534"}></Trans></span>
-                      <span><Trans i18nKey={"domDrammen.4581"}></Trans></span>
+                      <span className="tile"><Trans i18nKey={"lazMoss.4534"}></Trans></span>
+                      <span><Trans i18nKey={"lazMoss.4581"}></Trans></span>
                     </li>
                     <li>
-                      <span className="tile"><Trans i18nKey={"domDrammen.4701"}></Trans></span>
-                      <span><Trans i18nKey={"domDrammen.4751"}></Trans></span>
+                      <span className="tile"><Trans i18nKey={"lazMoss.4701"}></Trans></span>
+                      <span><Trans i18nKey={"lazMoss.4751"}></Trans></span>
                     </li>
                   </ul>
                 </div>
                 <div className="col-md-12">
                   <div className="tags">
-                    <span className="heading"><Trans i18nKey={"domDrammen.4984"}></Trans></span>
-                    <span className="content"><Trans i18nKey={"domDrammen.5045"}></Trans></span>
-                    <span className="content"><Trans i18nKey={"domDrammen.5111"}></Trans></span>
-                    <span className="content"><Trans i18nKey={"domDrammen.5175"}></Trans></span>
-                    <span className="content"><Trans i18nKey={"domDrammen.5238"}></Trans></span>
+                    <span className="heading"><Trans i18nKey={"lazMoss.4984"}></Trans></span>
+                    <span className="content"><Trans i18nKey={"lazMoss.5045"}></Trans></span>
+                    <span className="content"><Trans i18nKey={"lazMoss.5111"}></Trans></span>
+                    <span className="content"><Trans i18nKey={"lazMoss.5175"}></Trans></span>
+                    <span className="content"><Trans i18nKey={"lazMoss.5238"}></Trans></span>
                   </div>
                 </div>
                 <div className="col-md-12">
                   <div className="share">
-                    <span className="heading"><Trans i18nKey={"domDrammen.5438"}></Trans></span>
+                    <span className="heading"><Trans i18nKey={"lazMoss.5438"}></Trans></span>
                     <span className="content">
                       <i className="fa fa-facebook-f"></i>
                     </span>
